1f5dd94461fa752675f45f5b6a6ade9289f76b5d
jhermysam <150271844+jhermysam@users.noreply.github.com>
2023-11-29 20:19:11 -0500

    Create Saludo.java

---
+++ Saludo.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.saludo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Saludo
+
+{
+
+public static void main( String[] args)
+
+{
+
+System.out.println("Saludos desde Sangolqui ");
+
+ 
+
+//Fin del metodo main
+
+  }
+
+ }
+
+//Fin de la clase Saludo 
